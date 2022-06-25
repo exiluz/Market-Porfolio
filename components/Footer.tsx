97deadc4c66b441140c6bc29c6acc397e85fc57f
elyxus <81509423+ElyxusPH@users.noreply.github.com>
2022-06-25 23:06:34 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,21 +10,10 @@ const Footer = ({ showSkills }: { showSkills: boolean }) => {
 
       <div className="w-full bg-neutral-800 py-24 text-white">
         <div className="container text-center">
-          <h2 className="mb-10">Wanna work with me?</h2>
+          <h2 className="mb-10">Having Suggestions?</h2>
 
-          <div className="flex flex-col md:flex-row justify-center items-center space-y-3 md:space-y-0 md:space-x-5">
-            <Button
-              href="https://twitter.com/youUsername"
-              target="_blank"
-              icon="fa-solid:paper-plane"
-            >
-              DM me on Twitter
-            </Button>
-
-            <span>or</span>
-
-            <Button href="mailto:your@email.com" icon="fa-solid:envelope">
-              Send me an email
+            <Button href="mailto:support@sixty9unit.tech" icon="fa-solid:envelope">
+              Send me an Email
             </Button>
           </div>
 
